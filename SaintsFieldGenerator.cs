bb240180db82f2797215f18decc520e267953105
TylerTemp <tylertempdev@gmail.com>
2025-09-23 09:44:28 +0100

    init

---
+++ SaintsFieldGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace SaintsFieldSourceGenerator
+{
+    [Generator]
+    public class SaintsFieldGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            System.Console.WriteLine(System.DateTime.Now.ToString());
+
+            var sourceBuilder = new StringBuilder(
+            @"
+            using System;
+            namespace ExampleSourceGenerated
+            {
+                public static class ExampleSourceGenerated
+                {
+                    public static string GetTestText()
+                    {
+                        return ""This is from source generator ");
+
+            sourceBuilder.Append(System.DateTime.Now.ToString());
+
+            sourceBuilder.Append(
+                @""";
+                    }
+    }
+}
+");
+
+            context.AddSource("exampleSourceGenerator", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
+
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+    }
+}
